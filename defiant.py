364bc9ce851f894e0f9bcaa67ff954658139fcd4
tkrupicka <tylerkrupicka@gmail.com>
2014-09-20 21:39:13 -0400

    stores previous posts, ranking

--- defiant.py
+++ defiant.py
@@ -24,58 +24,55 @@ class Defiant:
         self.timerRunning = False
         self.ready = False
         self.delay = 120.0
+        self.last = []
+        self.record = 0
 
     def main(self):
     	while True:
             if self.timerRunning == False:
-                print "starting Timer"
+                self.ready = False
                 self.startTimer()
             self.pollForTweets()
             self.famous()
-            print len(self.tweets)
             if self.ready == True or self.count == 0:
                 for tweet in self.tweets:
-                    if "definitely" in tweet.text:
-                        if("difference between" in tweet.text
-                            or "hate" in tweet.text or "spell" in tweet.text
-                                or "mean" in tweet.text or "differ" in tweet.text):
-                            self.postThanks(tweet)
+                    if tweet.text not in self.last:
+                        if "definitely" in tweet.text:
+                            if("difference between" in tweet.text
+                                or "hate" in tweet.text or "spell" in tweet.text
+                                    or "mean" in tweet.text or "differ" in tweet.text):
+                                self.postThanks(tweet)
+                                break
+                        else:
+                            self.postCorrection(tweet)
                             break
-                    else:
-                        self.postCorrection(tweet)
-                        break
 
-            print "waiting!"
-            time.sleep(30)
+            time.sleep(15)
 
     def startTimer(self):
         #start the timer and set to toggleReady after delay
-        t = threading.Timer(self.delay,self.toggleReady)
+        t = threading.Timer(self.delay,self.setReady)
         self.timerRunning = True
         t.daemon = True
         t.start()
-        print "timer started"
 
-    def toggleReady(self):
+    def setReady(self):
         #toggle ready to post depending on value
-        if self.ready == False:
-            self.ready == True
-            print "ready!"
-        else:
-            self.ready == False
+            self.ready = True
 
     def pollForTweets(self):
         #add the current pulling tweets with lowered text
         #to the array for processing for the next post
-        currentPoll = api.search(q = self.query, rpp = 1)
-        print "polling!"
+        currentPoll = api.search(q = self.query, rpp = 100)
         for tweet in currentPoll:
             tweet.text = tweet.text.lower()
             self.tweets.append(tweet)
         print len(self.tweets)
+        self.tweets = self.tweets[0:100]
 
     def postThanks(self,tweet):
         #post a thank you message to the user
+        self.store(tweet.text)
         sn = tweet.user.screen_name
         if(len(tweet.text)+len(sn)<=97):
             message =  'Thank you for knowing the difference RT "'
@@ -90,6 +87,7 @@ class Defiant:
     def postCorrection(self, tweet):
         #post the standard correction message in reply
         #to the tweet
+        self.store(tweet.text)
         sn = tweet.user.screen_name
         message = "@%s " % (sn)
         message +=  "Did you mean definitely?"
@@ -100,14 +98,20 @@ class Defiant:
         #add to count, reset the ready timer, print status,
         #reset tweets to analyze
         self.count += 1
-        self.toggleReady()
-        print (message + "  Count: " + str(self.count))
+        self.ready = False
+        print (message + "  Count: " + str(self.count) + " Record: " + str(self.record))
         self.tweets = []
         self.timerRunning = False
 
     def famous(self):
-        print "sorting"
         self.tweets = sorted(self.tweets, key=lambda tweet: tweet.user.followers_count, reverse=True)
+        best = self.tweets[0]
+        if best.user.followers_count > self.record:
+            self.record = best.user.followers_count
+
+    def store(self,tweet):
+        self.last.append(tweet)
+        self.last = self.last[0:100]
 
 if __name__ == '__main__':
     #run program main
