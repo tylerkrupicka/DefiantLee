353b9789d1cbecfaab27d35a12d14341b5e8bb3d
tkrupicka <tylerkrupicka@gmail.com>
2014-09-26 19:15:29 -0400

    bug fix for duplicate status, now checks past usernames.some minor improvements

--- defiant.py
+++ defiant.py
@@ -17,41 +17,43 @@ class Defiant:
 
     def __init__(self):
         #initialize class variables
-        self.version = 1.5
+        self.name = "@DefiantLee_"
         self.query = 'defiantly'
         self.count = 0
         self.tweets = []
-	self.tweetsText = []
         self.timerRunning = False
         self.ready = False
-        self.delay = 120.0
-        self.last = []
+        self.delay = 90.0
+        self.lastUsers = []
         self.record = 0
-	self.recordHolder = " "
-	self.corpusFile = "corpus.txt"
-	self.recordFile = "records.txt"
+        self.recordHolder = " "
+        self.corpusFile = "corpus.txt"
+        self.recordFile = "records.txt"
+        #swear words to NOT retweet
+        self.swear = ["fuck","shit","bitch","ass"]
 
     def main(self):
-    	while True:
+        while True:
             if self.timerRunning == False:
                 self.ready = False
                 self.startTimer()
+            
             self.pollForTweets()
             self.famous()
             if self.ready == True or self.count == 0:
                 #print len(self.tweets)
-		for tweet in self.tweets:
-                	if "definitely" in tweet.text:
-                            	if("difference between" in tweet.text
-                                	or "hate" in tweet.text or "spell" in tweet.text
-                                    		or "mean" in tweet.text or "differ" in tweet.text):
-                                	self.postThanks(tweet)
-                                	break
-                        else:
-                            self.postCorrection(tweet)
-                            break
+                for tweet in self.tweets:
+                    if "definitely" in tweet.text:
+                                if("difference between" in tweet.text
+                                    or "hate" in tweet.text or "spell" in tweet.text
+                                            or "mean" in tweet.text or "differ" in tweet.text):
+                                    self.postThanks(tweet)
+                                    break
+                    else:
+                        self.postCorrection(tweet)
+                        break
 
-            time.sleep(20)
+            time.sleep(10)
 
     def startTimer(self):
         #start the timer and set to toggleReady after delay
@@ -67,30 +69,36 @@ class Defiant:
     def pollForTweets(self):
         #add the current pulling tweets with lowered text
         #to the array for processing for the next post
-        currentPoll = [status for status in tweepy.Cursor(api.search, q=self.query).items(10)]
+        currentPoll = [status for status in tweepy.Cursor(api.search, q=self.query).items(20)]
         for tweet in currentPoll:
             tweet.text = tweet.text.lower()
-            if tweet.text in self.last or tweet.text in self.tweetsText:
-		pass 
-	    elif hasattr(tweet, 'retweeted_status'):
-		pass
-	    elif "rt @" in tweet.text:
-		pass
-	    else:
-	        self.tweets.append(tweet)
-		self.tweetsText.append(tweet.text)
+            if tweet.user.screen_name in self.lastUsers:
+                pass 
+            elif hasattr(tweet, 'retweeted_status'):
+                pass
+            elif "rt @" in tweet.text:
+                pass
+            elif self.name in tweet.text:
+                pass
+            else:
+                self.tweets.append(tweet)
 
     def postThanks(self,tweet):
         #post a thank you message to the user
-        self.store(tweet.text)
+        self.store(tweet.user.screen_name)
         sn = tweet.user.screen_name
+
+        for word in self.swear:
+            if word in tweet.text:
+                tweet.text.replace(word, "*")
+
         if(len(tweet.text)+len(sn)<=70):
             message =  'Thank you for knowing the difference RT "'
         else:
             message = 'Thank You RT "'                          
         message += "@%s " % (sn)
         message += tweet.text + ' "'
-	message = message[0:140]        
+        message = message[0:140]        
         api.update_status(message,tweet.id)
         self.afterPost(message)
 
@@ -98,14 +106,14 @@ class Defiant:
     def postCorrection(self, tweet):
         #post the standard correction message in reply
         #to the tweet
-        self.store(tweet.text)
+        self.store(tweet.user.screen_name)
         sn = tweet.user.screen_name
         message = "@%s " % (sn)
 
-	f = open(self.corpusFile, 'a')
-	save =  message + tweet.text + ' \n'
-	f.write(save.encode('utf8')) 
-	f.close()
+        f = open(self.corpusFile, 'a')
+        save =  message + tweet.text + ' \n'
+        f.write(save.encode('utf8')) 
+        f.close()
 
         message +=  "Did you mean definitely?"
         api.update_status(message,tweet.id)
@@ -119,7 +127,7 @@ class Defiant:
         self.ready = False
         print (message + "  Count: " + str(self.count) + " Record: " + str(self.record) + " Holder: " + str(self.recordHolder))
         self.tweets = []
-	self.tweetsText = []
+        self.tweetsText = []
         self.timerRunning = False
 
     def famous(self):
@@ -127,16 +135,17 @@ class Defiant:
         best = self.tweets[0]
         if best.user.followers_count > self.record:
             self.record = best.user.followers_count
-	    self.recordHolder = best.user.screen_name
-	if best.user.followers_count > 100000 and best.text not in self.tweetsText:
-		save = best.user.screen_name + " Followers: " + str(best.user.followers_count) + "\n"
-		f = open(self.recordFile, 'a')
-		f.write(save.encode('utf8'))
-		f.close()
+            self.recordHolder = best.user.screen_name
 
-    def store(self,tweet):
-        self.last.append(tweet)
-        self.last = self.last[0:100]
+        if best.user.followers_count > 100000 and best.user.screen_name not in self.lastUsers:
+            save = best.user.screen_name + " Followers: " + str(best.user.followers_count) + "\n"
+            f = open(self.recordFile, 'a')
+            f.write(save.encode('utf8'))
+            f.close()
+
+    def store(self,user):
+        self.lastUsers.append(user)
+        self.lastUsers = self.lastUsers[0:100]
 
 if __name__ == '__main__':
     #run program main
